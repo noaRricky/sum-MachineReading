c5a218836e8412c652f62ce7d1a2d24095daf03a
NoaRicky <480582562@qq.com>
2018-08-12 09:52:38 +0800

    build base token class

--- tokenizers/tokenizer.py
+++ tokenizers/tokenizer.py
@@ -1,6 +1,125 @@
 """Base tokenizer/tokens classes and utilities
 """
 
+import copy
+
+
+class Tokens(object):
+    """a class to represent as list of tokenized text."""
+    TEXT = 0
+    TEXT_WS = 1
+    SPAN = 2
+    POS = 3
+    LEMMA = 4
+    NER = 5
+
+    def __init__(self, data, annotators, opts=None):
+        self.data = data
+        self.annotators = annotators
+        self.opts = opts or {}
+
+    def __len__(self):
+        return len(self.data)
+
+    def slice(self, i=None, j=None):
+        """Return a view of the list of tokes from [i, j)"""
+        new_tokens = copy.copy(self)
+        new_tokens.data = self.data[i: j]
+        return new_tokens
+
+    def untokenize(self):
+        """Return the original text (with whitespace reinserted)"""
+        return ''.join([t[self.TEXT_WS] for t in self.data]).strip()
+
+    def words(self, uncased=False):
+        """Return a list of the text of each toke
+
+        Keyword Arguments:
+            uncased {bool} -- lower case text (default: {False})
+        """
+        if uncased:
+            return [t[self.TEXT].lower() for t in self.data]
+        else:
+            return [t[self.TEXT] for t in self.data]
+
+    def offsets(self):
+        """Returns a list of [start, end) character offset of each token.
+        """
+        return [t[self.SPAN] for t in self.data]
+
+    def pos(self):
+        """Returns a list of part-of-speech tags of each token.
+        Returns None if this annotation was not included.
+        """
+        if 'pos' not in self.annotators:
+            return None
+        return [t[self.POS] for t in self.data]
+
+    def lemmas(self):
+        """Returns a list of the lemmatized text of each token.
+        Returns None if this annotation was not included.
+        """
+        if 'lemma' not in self.annotators:
+            return None
+        return [t[self.LEMMA] for t in self.data]
+
+    def entities(self):
+        """Returns a list of named-entity-recognition tags of each token.
+        Returns None if this annotation was not included.
+        """
+        if 'ner' not in self.annotators:
+            return None
+        return [t[self.NER] for t in self.data]
+
+    def ngram(self, n=1, uncased=False, filter_fn=None, as_strings=True):
+        """Return a list of all ngrams from length 1 to n.
+
+        Keyword Arguments:
+            n {int} -- upper limit of ngram length (default: {1})
+            uncased {bool} -- lower cases test (default: {False})
+            filter_fn {function} -- user function that takes in an ngram list and returns
+                True or False to keep or not keep the ngram (default: {None})
+            as_strings {bool} -- return the ngram as a string vs list (default: {True})
+        """
+        def _skip(gram):
+            if not filter_fn:
+                return False
+            return filter_fn(gram)
+
+        words = self.words(uncased)
+        ngrams = [(s, e + 1)
+                  for s in range(len(words))
+                  for e in range(s, min(s + n, len(words)))
+                  if not _skip(words[s:e + 1])]
+
+        # concateate into strings
+        if as_strings:
+            ngrams = ['{}'.format(' '.join(words[s:e])) for (s, e) in ngrams]
+
+        return ngrams
+
+    def entity_groups(self):
+        """Group consecutive entity tokens with the same NSR tag.
+        """
+        entities = self.entities()
+        if not entities:
+            return None
+        non_ent = self.opts.get('non_ent', 'o')
+        groups = []
+        idx = 0
+        while idx < len(entities):
+            ner_tag = entities[idx]
+            # Check for entity tag
+            if ner_tag != non_ent:
+                # Champ the sequence
+                start = idx
+                while (idx < len(entities) and entities[idx] == ner_tag):
+                    idx += 1
+                groups.append((self.slice(start, idx).untokenize(), ner_tag))
+            else:
+                idx += 1
+        return groups
+
 
 class Tokenzier(object):
     """Base tokenizer class
