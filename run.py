3a08faeadf5b210dd0e1abbb1ef93b127071649f
NoaRicky <480582562@qq.com>
2018-08-14 18:27:57 +0800

    build dictionary

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import json
 
-from pynlpir_tokenizer import Tokenzier
+import pynlpir
+from gensim.corpora import Dictionary
 
 ARTICLE_ID = 'article_id'
 ARTICLE_TITLE = 'article_title'
@@ -11,22 +12,74 @@ QUESTIONS_ID = 'questions_id'
 ANSWER = 'answer'
 
 
-def extract_text(data: dict):
-    """extract text from data
+def build_dictionary(file_path: str):
+    """build token2id dictionary by the data in file_path
 
     Arguments:
-        data {dict} -- dictionary type for saving data
+        file_path {str} -- path for data file
     """
-    for article in data:
-        article_id = article[ARTICLE_ID]
-        article_title = article[ARTICLE_TITLE]
-        article_content = article[ARTICLE_CONTENT]
-        for questions_obj in article[QUESTIONS]:
-            question = questions_obj[QUESTION]
-            answer = questions_obj[ANSWER] 
-            
+    data = None
+    with open(file_path, mode='r', encoding='utf-8') as fp:
+        data = json.load(fp)
+
+    # build dictionary
+    dictionary = Dictionary()
+    with pynlpir.open():
+        for article in data:
+            # extract article text
+            article_title = article[ARTICLE_TITLE]
+            article_content = article[ARTICLE_CONTENT]
+
+            # convert full str to half str
+            article_title = str_q2b(article_title)
+            article_content = str_q2b(article_content)
+
+            # segement text
+            segment1 = pynlpir.segment(article_title, pos_tagging=False)
+            segment2 = pynlpir.segment(article_content, pos_tagging=False)
+
+            # remove empty item
+            segment1 = segment1.remove(' ')
+            segment2 = segment2.remove(' ')
+
+            dictionary.add_documents([segment1, segment2])
+
+            for questions_obj in article[QUESTIONS]:
+                # extract question text
+                question = questions_obj[QUESTION]
+                answer = questions_obj[ANSWER]
+
+                # convert full str to half string
+                question = str_q2b(question)
+                answer = str_q2b(answer)
+
+                # get token
+                segment1 = pynlpir.segment(question, pos_tagging=False)
+                segment2 = pynlpir.segment(answer, pos_tagging=False)
+
+                segment1 = segment1.remove(' ')
+                segment2 = segment2.remove(' ')
+                dictionary.add_documents([segment1, segment2])
+
+
+def str_q2b(ustring: str):
+    """convert full string to half string
+
+    Arguments:
+        string {str} -- unicode string
+    """
+    ret_str = ''
+    for uchar in ustring:
+        inside_code = ord(uchar)
+        if inside_code == 12288:
+            inside_code = 32
+        elif inside_code >= 65281 and inside_code <= 65374:
+            inside_code -= 65248
+
+        ret_str += chr(inside_code)
+    return ret_str
 
 
 if __name__ == '__main__':
-    with open("../data/question.json", mode='r', encoding='utf-8') as data_fp:
-        data = json.load(data_fp)
+    text_str = "ａｂｃ－＋－-ＡＢＣ１２３你好世界，。！、"
+    print(str_q2b(text_str))
