e28195f59b09da84d642d9f648f8577326b34519
NoaRicky <480582562@qq.com>
2018-10-24 20:23:42 +0800

    add scaleddotproduct attention

--- new/MwAN/attens.py
+++ new/MwAN/attens.py
@@ -1,5 +1,40 @@
+import numpy as np
 import torch
 import torch.nn as nn
+
+
+class ScaledDotProductAtten(nn.Module):
+    """Scaled dot-product attention mechainsm
+    """
+
+    def __init__(self, scale=None, atten_drop=0.1):
+        super(ScaledDotProductAtten, self).__init__()
+        self.scale = scale
+        self.dropout = nn.Dropout(atten_drop)
+        self.softmax = nn.Softmax(dim=2)
+
+    def forward(self, query, key, value, atten_mask=None):
+        """前向传播.
+
+        Args:
+                q: Queries张量，形状为[B, L_q, D_q]
+                k: Keys张量，形状为[B, L_k, D_k]
+                v: Values张量，形状为[B, L_v, D_v]，一般来说就是k
+                scale: 缩放因子，一个浮点标量
+                attn_mask: Masking张量，形状为[B, L_q, L_k]
+
+        Returns:
+                上下文张量和attetention张量
+        """
+        atten = torch.bmm(query, key.transpose(1, 2))
+        if atten_mask:
+            # 给需要mask的地方设置一个负无穷
+            atten.masked_fill_(atten_mask, -np.inf)
+        atten = self.softmax(atten)
+        # 添加dropout
+        atten = self.dropout(atten)
+        context = torch.bmm(atten, value)
+        return context, atten
 
 
 class ConcatAtten(nn.Module):
