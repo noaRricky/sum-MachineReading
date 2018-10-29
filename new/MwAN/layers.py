63851d1e94947952df2bdb5e1be41b3d4461e2cd
孙维松 <480582562@qq.com>
2018-10-29 21:48:54 +0800

    add model init part

--- new/MwAN/layers.py
+++ new/MwAN/layers.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from attens import MultiHeadAtten
+from attens import MultiHeadAtten, ScaledDotProductAtten
 
 
 class PositionwiseFeedForward(nn.Module):
@@ -33,7 +33,8 @@ class TransformerEncoder(nn.Module):
     def __init__(self, encode_size=512, num_heads=8, ffn_dim=2018, dropout=0.1):
         super(TransformerEncoder, self).__init__()
 
-        self.attention = MultiHeadAtten(encode_size, num_heads, dropout)
+        self.attention = MultiHeadAtten(ScaledDotProductAtten(
+            encode_size, dropout), encode_size, num_heads, dropout)
         self.feed_forward = PositionwiseFeedForward(
             encode_size, ffn_dim, dropout)
 
@@ -82,28 +83,31 @@ class EncodeLayer(nn.Module):
 
 class AggregateLayer(nn.Module):
 
-    def __init__(self, model_dim):
+    def __init__(self, encode_size):
         super(AggregateLayer, self).__init__()
 
-        self.gru_agg(6 * model_dim, model_dim,
+        model_dim = encode_size * 2
+        self.gru_agg(4 * model_dim, model_dim,
                      batch_first=True, bidirectional=True)
+        self.layer_norm = nn.LayerNorm(model_dim)
 
-    def forward(self, hp, qts, qtc, qtd, qtb, qtm):
-        aggregation = torch.cat([hp, qts, qtc, qtd, qtb, qtm], dim=2)
+    def forward(self, qtc, qtd, qtb, qtm):
+        aggregation = torch.cat([qtc, qtd, qtb, qtm], dim=2)
         aggregation_represent, _ = self.gru_agg(aggregation)
+        return self.layer_norm(aggregation_represent)
 
 
 class PredictionLayer(nn.Module):
 
-    def __init__(self, model_dim, dropout=0.1):
+    def __init__(self, encode_size, dropout=0.1):
         super(PredictionLayer, self).__init__()
 
-        self.Wq = nn.Linear(2 * model_dim, model_dim, bias=False)
-        self.vq = nn.Linear(model_dim, 1, bias=False)
-        self.Wp1 = nn.Linear(2 * model_dim, model_dim, bias=False)
-        self.Wp2 = nn.Linear(2 * model_dim, model_dim, bias=False)
-        self.vp = nn.Linear(model_dim, 1, bias=False)
-        self.prediction = nn.Linear(2 * model_dim, model_dim, bias=False)
+        self.Wq = nn.Linear(2 * encode_size, encode_size, bias=False)
+        self.vq = nn.Linear(encode_size, 1, bias=False)
+        self.Wp1 = nn.Linear(2 * encode_size, encode_size, bias=False)
+        self.Wp2 = nn.Linear(2 * encode_size, encode_size, bias=False)
+        self.vp = nn.Linear(encode_size, 1, bias=False)
+        self.prediction = nn.Linear(2 * encode_size, encode_size, bias=False)
         self.que_softmax = nn.Softmax(dim=2)
         self.agg_softmax = nn.Softmax(dim=2)
         self.score_softmax = nn.Softmax(dim=1)
--- new/MwAN/model.py
+++ new/MwAN/model.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 
-from layers import EncodeLayer
-from attens import ConcatAtten, BilinearAtten, DotAtten, MinusAtten
+from layers import EncodeLayer, AggregateLayer, PredictionLayer
+from attens import ConcatAtten, BilinearAtten, DotAtten, MinusAtten, MultiHeadAtten
 
 
 class MwANP(nn.Module):
@@ -25,3 +25,20 @@ class MwANP(nn.Module):
 
         # attention part
         # mult-head concat attention
+        self.mult_concat_atten = MultiHeadAtten(
+            ConcatAtten, encode_size, num_heads, dropout)
+        # multi-head bilinear attention
+        self.multi_bilinear_atten = MultiHeadAtten(
+            BilinearAtten, encode_size, num_heads, dropout)
+        # multi-head dotatten attention
+        self.multi_dot_atten = MultiHeadAtten(
+            DotAtten, encode_size, num_heads, dropout)
+        # multi-head minus attention
+        self.multi_minus_atten = MultiHeadAtten(
+            MinusAtten, encode_size, num_heads, dropout)
+
+        # aggregation part
+        self.agg_layer = AggregateLayer(encode_size)
+
+        # predict layer
+        self.predict_layer = PredictionLayer(encode_size, dropout)
